d9a4a123029fedac7ffe80735ab8fe3fc916f87e
98prateeksharma <prateek.sharma_cs18@gla.ac.in>
2020-02-24 09:26:16 +0530

    class added:PersonNode

---
+++ src/PersonNode.java
@@ -0,0 +1,14 @@
+public class PersonNode {
+    private Person person;
+    private PersonNode next;
+
+    public PersonNode(Person person) {
+        this.person = person;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+
+}
