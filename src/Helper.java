da15087b5aa8fda2952c588b272326216f5da624
98prateeksharma <prateek.sharma_cs18@gla.ac.in>
2020-02-24 09:37:26 +0530

    method added:add(PersonNode person)

--- src/Helper.java
+++ src/Helper.java
@@ -3,6 +3,25 @@ public class Helper {
     private PersonNode tail;
     private int size;
 
+    public void add(PersonNode person) {
+        if (isEmpty() || person.getPerson().getFirst().toLowerCase().compareTo(head.getPerson().getFirst().toLowerCase()) <= 0) {
+            person.setNext(head);
+            head = person;
+            size++;
+        } else {
+            PersonNode current = head;
+            PersonNode previous = null;
+            while (current != null && person.getPerson().getFirst().toLowerCase().compareTo(current.getPerson().getFirst().toLowerCase()) >= 0) {
+                previous = current;
+                current = current.getNext();
+            }
+            person.setNext(previous.getNext());
+            previous.setNext(person);
+            size++;
+        }
+    }
+
+
     public boolean isEmpty() {
         return head == null;
     }
